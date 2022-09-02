dd77bfe97a7c67a679e9611772824147d65b587d
Emanuele Correa <manuncorrea@hotmail.com>
2022-09-02 15:18:57 -0300

    criando rota publica

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,41 @@
-import {Routes, Route } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { Characters } from '../pages/Characters';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { Spaceships } from '../pages/Spaceship';
 
+//@ts-ignore
+const PublicRoute = ({ children, redirectTo }) => {
+  const isAuthenticated = localStorage.getItem('user_token') === null;
+  console.log('isAuth: ', isAuthenticated);
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+}
 
 export function AppRoutes() {
-  
+
 
   return (
-      <Routes>
-        <Route path='/' element={<Characters />} />
-        <Route path='/nave-espacial' element={<Spaceships />} />
+    <Routes>
+      <Route path='/' element={<Characters />} />
+      <Route path='/nave-espacial' element={<Spaceships />} />
 
-        <Route path='/login' element={<SignIn />} />
-        <Route path='/cadastrar' element={<SignUp />} />
-      </Routes>
+      <Route path='/login'
+
+        element={
+          <PublicRoute redirectTo='/'>
+            <SignIn />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path='/cadastrar'
+        element={
+          <PublicRoute redirectTo='/'>
+            <SignUp />
+          </PublicRoute>
+        }
+      />
+    </Routes>
   );
 }
